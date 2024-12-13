f6e393de2fce4507faee0a1810b188e5189a3c5c
antek1085 <antek1085@o2.pl>
2024-12-13 15:38:39 +0100

    First lag fix on Spell Cast

--- Assets/Sandbox/Antek/Brush/ScreenShotHandler.cs
+++ Assets/Sandbox/Antek/Brush/ScreenShotHandler.cs
@@ -10,6 +10,7 @@ public class ScreenShotHandler : MonoBehaviour
     static ScreenShotHandler instance;
     public NeuralNetworkImageCheck runModel;
     [SerializeField] RawImage image;
+    private Texture2D texture2D;
     int pngNumber;
     string path;
     
@@ -17,6 +18,7 @@ public class ScreenShotHandler : MonoBehaviour
     {
         instance = this;
         path = Directory.GetCurrentDirectory() + "/Assets"+"/Screenshots";
+        texture2D = new Texture2D(512, 512, TextureFormat.RGB24, false);
         if (Directory.Exists(path) == false)
         {
             Directory.CreateDirectory(path);
@@ -27,24 +29,24 @@ public class ScreenShotHandler : MonoBehaviour
     {
         yield return new WaitForEndOfFrame();
         Texture texture = image.texture;
-        Texture2D texture2D;
         
         RenderTexture renderTexture = RenderTexture.GetTemporary(
         texture.width, texture.height, 0, RenderTextureFormat.Default, RenderTextureReadWrite.Linear);
+        /*texture2D.width = texture.width;
+        texture2D.height = texture.height;*/
             
         Graphics.Blit(texture, renderTexture);
         RenderTexture previous = RenderTexture.active;
         RenderTexture.active = renderTexture;
-
-        texture2D = new Texture2D(texture.width, texture.height, TextureFormat.RGB24, false);
+        
         texture2D.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
         texture2D.Apply();
 
         RenderTexture.active = previous;
         RenderTexture.ReleaseTemporary(renderTexture);
         
-        byte[] byteArray = texture2D.EncodeToPNG();
-        System.IO.File.WriteAllBytes( path + "/cameraScreenshot"+ pngNumber +".png" , byteArray);
+        /*byte[] byteArray = texture2D.EncodeToPNG();*/
+        /*System.IO.File.WriteAllBytes( path + "/cameraScreenshot"+ pngNumber +".png" , byteArray);*/
         runModel.RunModel(texture2D);
         Debug.Log("Save");
     }
