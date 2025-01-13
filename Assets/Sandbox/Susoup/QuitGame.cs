831c75a0af505b65ca87071ca4b9aa2381bc57b3
Zuzanna Pawlicka <zuzanna2002pawlicka@gmail.com>
2025-01-13 15:13:10 +0100

    add main menu ui and options and fireVFX

---
+++ Assets/Sandbox/Susoup/QuitGame.cs
@@ -0,0 +1,14 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    public void Quit()
+    {
+
+        Application.Quit(); // this quits the game.
+
+    }
+    
+}
+
