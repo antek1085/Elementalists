87a70942a1c8ea2dd18dc540fceb1d12ecbaa2d2
Jakub Jóskowiak <jjoskowiak@edu.cdv.pl>
2025-01-21 18:44:58 +0100

    Fox on Scene

--- Assets/Sandbox/Jakub/Scripts/DeathZone.cs
+++ Assets/Sandbox/Jakub/Scripts/DeathZone.cs
@@ -2,18 +2,15 @@ using UnityEngine;
 
 public class DeathZone : MonoBehaviour
 {
-    public Transform transportTarget; // The target position to transport the player to
+    public Transform transportTarget;
 
     private void OnTriggerEnter(Collider other)
     {
-        // Check if the object entering the trigger has a Rigidbody (or any other specific component)
         Rigidbody playerRigidbody = other.GetComponent<Rigidbody>();
-        if (playerRigidbody != null) // This ensures we're interacting with an object with a Rigidbody
+        if (playerRigidbody != null) 
         {
-            // Transport the player to the target position
             other.transform.position = transportTarget.position;
-
-            // Optional: Reset the Rigidbody velocity
+            
             playerRigidbody.linearVelocity = Vector3.zero;
 
             Debug.Log($"{other.name} transported to {transportTarget.position}");
