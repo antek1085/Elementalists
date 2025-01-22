963ba1f0ba8b326d28bc3142ceec953d33bae93e
antek1085 <antek1085@o2.pl>
2025-01-22 19:41:10 +0100

    Casting text added

--- Assets/Sandbox/Antek/Brush/SpellsPlayerFeedbackUI.cs
+++ Assets/Sandbox/Antek/Brush/SpellsPlayerFeedbackUI.cs
@@ -1,10 +1,13 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpellsPlayerFeedbackUI : MonoBehaviour
 {
     [SerializeField] SO_FloatMinMAx spellCastThreshold;
-    [SerializeField] TextMeshProUGUI fireSpell,waterSpell;
+    [SerializeField] Image fireSpell,waterSpell;
+    [SerializeField] Gradient fireSpellGradient,waterSpellGradient;
+    [SerializeField] TextMeshProUGUI fireSpellText,waterSpellText;
     void Start()
     {
         DrawingSpellsEvent.current.OnSpellFloatUI += OnSpellFloatUI;
@@ -13,8 +16,27 @@ public class SpellsPlayerFeedbackUI : MonoBehaviour
     // Fire Spell 0  ||| Water Spell 1
     void OnSpellFloatUI(float[] obj)
     {
-        fireSpell.text = obj[0].ToString();
-        waterSpell.text = obj[1].ToString();
+        Debug.Log(obj[0] + "Fire");
+        Debug.Log(obj[1] + "Water");
+        fireSpell.color = fireSpellGradient.Evaluate(obj[1]);
+        waterSpell.color = waterSpellGradient.Evaluate(obj[0]);
+
+        if (spellCastThreshold.value < obj[0])
+        {
+            fireSpellText.enabled = true;
+        }
+        else
+        {
+            fireSpellText.enabled = false;
+        }
+        if (spellCastThreshold.value < obj[1])
+        {
+            waterSpellText.enabled = true;
+        }
+        else
+        {
+            waterSpellText.enabled = false;
+        }
     }
 
     // Update is called once per frame
