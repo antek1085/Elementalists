c497ef7b96d127b8482691dcd2ae5abe0bb86b85
Jakub Jóskowiak <jjoskowiak@edu.cdv.pl>
2025-01-26 21:45:57 +0100

    Dialogue Popup Placement + Sound

--- Assets/Sandbox/Antek/AnimalUI/DialogueTextBoxActivate.cs
+++ Assets/Sandbox/Antek/AnimalUI/DialogueTextBoxActivate.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using FMODUnity;
 using UnityEngine;
 
 public class DialogueTextBoxActivate : MonoBehaviour
@@ -7,11 +8,17 @@ public class DialogueTextBoxActivate : MonoBehaviour
     [SerializeField] string animalName;
     [SerializeField] List<float> time;
     [SerializeField] List<string> dialogue;
-    
+
+    [Header("Sound Settings")] [SerializeField]
+    private EventReference dialoguePopupSound;
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            if (dialoguePopupSound.IsNull == false)
+            {
+                RuntimeManager.PlayOneShot(dialoguePopupSound);
+            }
             AnimalUiEvents.current.DialogueStringPopUp(dialogue,time,animalName);
         }
         Destroy(this.gameObject);
