052b4d91a9e3f43e4018a4adabf6733f9727c7a1
antek1085 <antek1085@o2.pl>
2025-03-29 14:02:31 +0100

    NewNeuralNetwork

--- Assets/Sandbox/Antek/Brush/ScreenShotHandler.cs
+++ Assets/Sandbox/Antek/Brush/ScreenShotHandler.cs
@@ -18,6 +18,12 @@ public class ScreenShotHandler : MonoBehaviour
     {
         instance = this;
         texture2D = new Texture2D(512, 512, TextureFormat.RGB24, false);
+        
+        path = Directory.GetCurrentDirectory() + "/Assets"+"/Screenshots";
+        if (Directory.Exists(path) == false)
+        {
+            Directory.CreateDirectory(path);
+        }
     }
     void OnEnable()
     {
@@ -71,11 +77,36 @@ public class ScreenShotHandler : MonoBehaviour
     {
         StopAllCoroutines();
         StartCoroutine(CoroutineScreenShot());
+        ScreenShot();
     }
 
     public static void TakeScreenshot_Static()
     {
         instance.TakeScreenshot();
     }
-    
+        
+    private void ScreenShot()
+    {
+        Texture texture = image.texture;
+        Texture2D texture2D;
+        
+        RenderTexture renderTexture = RenderTexture.GetTemporary(
+        texture.width, texture.height, 0, RenderTextureFormat.Default, RenderTextureReadWrite.Linear);
+            
+        Graphics.Blit(texture, renderTexture);
+        RenderTexture previous = RenderTexture.active;
+        RenderTexture.active = renderTexture;
+
+        texture2D = new Texture2D(texture.width, texture.height, TextureFormat.RGB24, false);
+        texture2D.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+        texture2D.Apply();
+
+        RenderTexture.active = previous;
+        RenderTexture.ReleaseTemporary(renderTexture);
+        
+        byte[] byteArray = texture2D.EncodeToPNG();
+        System.IO.File.WriteAllBytes( path + "/cameraScreenshot"+ pngNumber +".png" , byteArray);
+        pngNumber += 1;
+        Debug.Log("Save");
+    }
 }
