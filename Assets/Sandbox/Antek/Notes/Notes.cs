bbe89f963624abc9b5508763aaf7fc19478d37d5
Jakub Jóskowiak <jjoskowiak@edu.cdv.pl>
2025-05-17 12:48:18 +0200

    Notes script change to display gameobject instead of dialogue
    + Journal spell descriptions

--- Assets/Sandbox/Antek/Notes/Notes.cs
+++ Assets/Sandbox/Antek/Notes/Notes.cs
@@ -1,29 +1,69 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Notes : MonoBehaviour
 {
-   NotesController _notesController;
-   AnimalUiEvents _animalUiEvents;
-   
-   [SerializeField] string noteDescription;
-   [SerializeField] List<String> notes = new List<String>();
-   [SerializeField] List<float> noteDurations = new List<float>();
-   public void AddNote()
-   {
-      _notesController = NotesController.instance;
-      _animalUiEvents = AnimalUiEvents.current;
-      
-      
-      _notesController.AddNote(noteDescription);
-      StartCoroutine(notesDialogue());
-   }
+    NotesController _notesController;
+    [SerializeField] string noteDescription;
+    [SerializeField] GameObject noteDisplayObject; 
+    [SerializeField] float displayDuration = 3f; 
 
-   IEnumerator notesDialogue()
-   {
-      yield return new WaitForSeconds(10f);
-      _animalUiEvents.DialogueStringPopUp(notes,noteDurations,"");
-   }
+    void Start()
+    {
+        _notesController = NotesController.instance;
+        
+        if (noteDisplayObject != null)
+        {
+            noteDisplayObject.SetActive(false);
+        }
+    }
+
+    public void AddNote()
+    {
+        _notesController.AddNote(noteDescription);
+        StartCoroutine(ShowAndHideNote());
+    }
+
+    IEnumerator ShowAndHideNote()
+    {
+        if (noteDisplayObject != null)
+        {
+            noteDisplayObject.SetActive(true);
+            yield return new WaitForSeconds(displayDuration);
+            noteDisplayObject.SetActive(false);
+        }
+    }
 }
+
+/*using System;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    NotesController _notesController;
+    AnimalUiEvents _animalUiEvents;
+   
+    [SerializeField] string noteDescription;
+    [SerializeField] List<String> notes = new List<String>();
+    [SerializeField] List<float> noteDurations = new List<float>();
+    public void AddNote()
+    {
+        _notesController = NotesController.instance;
+        _animalUiEvents = AnimalUiEvents.current;
+      
+      
+        _notesController.AddNote(noteDescription);
+        StartCoroutine(notesDialogue());
+    }
+
+    IEnumerator notesDialogue()
+    {
+        yield return new WaitForSeconds(10f);
+        _animalUiEvents.DialogueStringPopUp(notes,noteDurations,"");
+    }
+}
+ */
