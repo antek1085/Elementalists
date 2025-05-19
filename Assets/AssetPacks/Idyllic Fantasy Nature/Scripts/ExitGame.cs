f2a462301abf4f58b3e76a171b41a859d7f44021
Jakub Jóskowiak <jjoskowiak@edu.cdv.pl>
2025-05-19 15:50:11 +0200

    build bugfix

--- Assets/AssetPacks/Idyllic Fantasy Nature/Scripts/ExitGame.cs
+++ Assets/AssetPacks/Idyllic Fantasy Nature/Scripts/ExitGame.cs
@@ -7,10 +7,10 @@ namespace IdyllicFantasyNature
         void Update()
         {
             // If you press the ESC key in the game, the application will be closed
-            if (Input.GetKey(KeyCode.Escape))
-            {
-                Application.Quit();
-            }
+            //if (Input.GetKey(KeyCode.Escape))
+            //{
+                //Application.Quit();
+            //}
         }
           public void QuitGame()
         {
