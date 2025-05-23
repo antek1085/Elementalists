68c5fdb7abb0c79253e93bbc62b437579f05fb98
susoupp <zuzanna2002pawlicka@gmail.com>
2025-05-23 12:15:06 +0200

    fixes journal

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    
+    public void LoadMenuScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
